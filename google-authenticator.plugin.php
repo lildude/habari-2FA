c38447bc6fe73595e150368497e7c72da5778d0a
Colin Seymour <lildood@gmail.com>
2012-08-07 11:46:25 +0100

    Added basic plugin files

---
+++ google-authenticator.plugin.php
@@ -0,0 +1,20 @@
+<?php
+class GoogleAuthenticator extends Plugin
+{
+	public function action_init( )
+	{
+		
+	}
+	
+	public function action_plugin_activation( )
+	{
+		
+	}
+	
+	public function action_plugin_deactivation( )
+	{
+		
+	}
+	
+}
+?>
